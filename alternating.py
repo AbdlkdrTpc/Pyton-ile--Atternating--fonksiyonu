ad44371391c55143324896252946613507dfe6cf
AbdlkdrTpc <kdrtpc@icloud.com>
2023-10-09 10:29:41 +0300

    3 farklı yöntem ile Bir string'inn harflerini büyütme veya küçültme işlemi yapan python fonksiyonu
    3 farklı yöntem ile Bir stringin indexlerinin çift veya tek sayı olmasına bağlı olarak ilgili indexteki değeri büyük harfe veya küçük harfe çeviren bir fonksiyon

---
+++ alternating.py
@@ -0,0 +1,34 @@
+
+##### Python process #####
+def alternating(string):
+    new_string = ""
+    for string_index in range(len(string)):
+        if string_index %2 == 0:
+            new_string += string[string_index].upper()
+        else:
+            new_string += string[string_index].lower()
+    print(new_string)
+alternating("bu benim ilk kod çalışmam")
+
+string = "bu benim ilk kod çalışmam"
+
+
+##### List comprehensions process#####
+x = [string[string_index].upper() if string_index %2 == 0 else string[string_index].lower() for string_index in range(len(string))]
+liste = ""
+for i in x:
+    liste += i
+print(liste)
+
+
+##### enumerate function #####
+def alternative(string):
+    liste = ""
+    for i, v in enumerate(string):
+        if i %2 == 0:
+            liste += v.upper()
+        else:
+            liste += v.lower()
+    print(liste)
+alternative("bu benim il kod çalışmam")
+
